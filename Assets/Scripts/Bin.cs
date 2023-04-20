f788980d41b06768fc62a46b5b2d32bfa651d6f2
Corentin Couasnon <ccouasno7@gmail.com>
2023-04-20 09:34:50 +0200

    Music

--- Assets/Scripts/Bin.cs
+++ Assets/Scripts/Bin.cs
@@ -6,7 +6,11 @@ public class Bin : MonoBehaviour
     [SerializeField] Player _owner;
     [SerializeField] ItemType _acceptedType;
     [SerializeField] int _capacity;
+    [SerializeField] AudioClip _correctSound;
+    [SerializeField] AudioClip _errorSound;
 
+    AudioSource _audioSource;
+    
     int _count;
 
     public Player Owner => _owner;
@@ -14,22 +18,31 @@ public class Bin : MonoBehaviour
     
     public static event Action<Bin> BinFull;
 
+    void Awake()
+    {
+        _audioSource = FindObjectOfType<AudioSource>();
+    }
+
     public void Accept(Player player, ItemData itemData)
     {
         if (player != _owner || itemData == null)
             return;
-        
-        if (itemData.ItemType == _acceptedType)
-        {
-            _count++;
-            _owner.Points++;
-            _owner.Pickup = null;
 
-            if (_count >= _capacity)
-            {
-                BinFull?.Invoke(this);
-                _count = 0;
-            }
+        if (itemData.ItemType != _acceptedType)
+        {
+            _audioSource.PlayOneShot(_errorSound);
+            return;
+        }
+
+        _count++;
+        _owner.Points++;
+        _owner.Pickup = null;
+        _audioSource.PlayOneShot(_correctSound);
+
+        if (_count >= _capacity)
+        {
+            BinFull?.Invoke(this);
+            _count = 0;
         }
     }
 }
